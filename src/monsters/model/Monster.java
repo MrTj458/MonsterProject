17854fee26b515fab286181f57851f744df74575
MrTj458 <thod0127@740s-pl-122.local>
2015-09-15 13:01:18 -0600

    dded getters and setters in monsters.model.Monster

--- src/monsters/model/Monster.java
+++ src/monsters/model/Monster.java
@@ -25,6 +25,79 @@ public class Monster
 		this.monsterHair = monsterHair;
 		this.monsterArms = monsterArms;
 	}
+	
+	//Getters
+	public String getMonsterName()
+	{
+		return monsterName;
+	}
+	
+	public int getMonsterEyes()
+	{
+		return monsterEyes;
+	}
+	
+	public int getMonsterNoses()
+	{
+		return monsterNoses;
+	}
+	
+	public double getMonsterArms()
+	{
+		return monsterArms;
+	}
+	
+	public double getMonsterLegs()
+	{
+		return monsterLegs;
+	}
+	
+	public double getMonsterHair()
+	{
+		return monsterHair;
+	}
+	
+	public boolean getMonsterBellyButton()
+	{
+		return monsterBellyButton;
+	}
+	
+	//Setters
+	public void setMonsterName(String monsterName)
+	{
+		this.monsterName = monsterName;
+	}
+	
+	public void setMonsterEyes(int monsterEyes)
+	{
+		this.monsterEyes = monsterEyes;
+	}
+	
+	public void setMonsterNoses(int monsterNoses)
+	{
+		this.monsterNoses = monsterNoses;
+	}
+	
+	public void setMonsterArms(double monsterArms)
+	{
+		this.monsterArms = monsterArms;
+	}
+	
+	public void setMonsterLegs(double monsterLegs)
+	{
+		this.monsterLegs = monsterLegs;
+	}
+	
+	public void setMonsterHair(double monsterHair)
+	{
+		this.monsterHair = monsterHair;
+	}
+	
+	public void setMonsterBellyButton(boolean monsterBellyButton)
+	{
+		this.monsterBellyButton = monsterBellyButton;
+	}
+	
 	public String toString()
 	{
 		String bbutton;
