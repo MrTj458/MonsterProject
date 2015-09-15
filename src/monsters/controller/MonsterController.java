8caabb802fc56d107b5a3ae5699f85ec663e57bc
MrTj458 <thod0127@740s-pl-122.local>
2015-09-15 13:57:17 -0600

    Finished Questions

--- src/monsters/controller/MonsterController.java
+++ src/monsters/controller/MonsterController.java
@@ -34,8 +34,46 @@ public class MonsterController
 	
 	private void askQuestions()
 	{
-		System.out.println("Enter a new name for your monster: ");
+		System.out.println("Enter a new name for your monster");
 		String newMonsterName = monsterScanner.next();
 		DwaneMonster.setMonsterName(newMonsterName);
+		
+		System.out.println("How many eyes will your new monster have?");
+		int newMonsterEyes = monsterScanner.nextInt();
+		DwaneMonster.setMonsterEyes(newMonsterEyes);
+		
+		System.out.println("How many noses will your new monster have?");
+		int newMonsterNoses = monsterScanner.nextInt();
+		DwaneMonster.setMonsterNoses(newMonsterNoses);
+		
+		System.out.println("How many legs will your monster have?");
+		double newMonsterLegs = monsterScanner.nextDouble();
+		DwaneMonster.setMonsterLegs(newMonsterLegs);
+		
+		System.out.println("How many arms will your monster have?");
+		double newMonsterArms = monsterScanner.nextDouble();
+		DwaneMonster.setMonsterLegs(newMonsterArms);
+		
+		System.out.println("How many hairs will your monster have?");
+		double newMonsterHairs = monsterScanner.nextDouble();
+		DwaneMonster.setMonsterLegs(newMonsterHairs);
+		
+		System.out.println("Will your monster have a belly button? [y/n]");
+		String usrBellyButton = monsterScanner.next();
+		boolean newMonsterBellyButton;
+		if (usrBellyButton.equals("y"))
+		{
+			newMonsterBellyButton = true;
+		}
+		else if (usrBellyButton.equals("n"))
+		{
+			newMonsterBellyButton = false;
+		}
+		else
+		{
+			System.out.println("Unknown response setting to false");
+			newMonsterBellyButton = false;
+		}
+		DwaneMonster.setMonsterBellyButton(newMonsterBellyButton);
 	}
 }
